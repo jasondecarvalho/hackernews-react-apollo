89e131dcc598955c74c0872ebc4cc65b9270784f
Jason de Carvalho <jasondecarvalho@gmail.com>
2018-11-04 13:15:21 +0000

    Add initial mutation for creating link

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import LinkList from './LinkList'
+import CreateLink from './CreateLink.js'
 
 class App extends Component {
   render() {
-    return <LinkList />
+    return <CreateLink />
   }
 }
 
